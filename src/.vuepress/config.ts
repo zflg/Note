c4c70e7102e43cde688958e6175cc236cb5ba420
57block <504643686@qq.com>
2023-01-31 15:11:35 +0800

    init vuepress

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,15 +2,15 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/Note/",
 
   locales: {
-    "/": {
+    "/Note/": {
       lang: "en-US",
       title: "Docs Demo",
       description: "A docs demo for vuepress-theme-hope",
     },
-    "/zh/": {
+    "/Note/zh/": {
       lang: "zh-CN",
       title: "文档演示",
       description: "vuepress-theme-hope 的文档演示",
